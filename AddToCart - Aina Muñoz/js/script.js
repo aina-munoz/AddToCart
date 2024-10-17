5aeaca416099250b9a45914087085cfd5b277e8c
aina-munoz <amunoz19587@iesjoanramis.org>
2024-10-17 08:44:40 +0200

    Add files via upload

--- AddToCart - Aina Muñoz/js/script.js
+++ AddToCart - Aina Muñoz/js/script.js
@@ -1,6 +1,18 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+
 const input = document.getElementById("inputField")
 const boto = document.getElementById("afegir");
 
+const appSettings = {
+    databaseURL: "https://addtocart-1c03d-default-rtdb.europe-west1.firebasedatabase.app/"
+}
+
+const app = initializeApp(appSettings);
+const baseDades = getDatabase(app);
+const tasks = ref(baseDades, "tareas");
+
 boto.addEventListener("click", function(){
-    console.log(input.value)
+    push(tasks, input.value)
+    alert("Afegit a la BD");
 })
